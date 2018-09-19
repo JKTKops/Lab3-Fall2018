d14fce38a0df002ffff854e38d9dd42a92651842
JKTKops <zerglingk9012@gmail.com>
2018-09-18 20:24:08 -0500

    made urlToString private

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -24,7 +24,7 @@ public class WebScraper {
      * @param url url to retrieve contents from
      * @return the contents from the url as a string, or an empty string on error
      */
-    public static String urlToString(final String url) {
+    private static String urlToString(final String url) {
         Scanner urlScanner;
         try {
             urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
